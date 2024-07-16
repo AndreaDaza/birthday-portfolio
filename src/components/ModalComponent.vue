6c95090d005e1024b4046e23d3e7b4ed66714469
Andrea <tangananica24@gmail.com>
2024-07-16 08:39:07 -0500

    chore: added more recommendations

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -12,16 +12,14 @@ defineProps({
 <template>
   <v-dialog activator="parent">
     <template v-slot:default="{ isActive }">
-      <v-card class="mx-auto" width="400" height="300">
+      <v-card class="mx-auto" width="500" max-height="400">
         <v-card-text class="pa-5 poppins-light"> {{ content.message }} </v-card-text>
-
         <v-card-actions>
           <v-btn class="ml-auto" text="Close" @click="isActive.value = false"></v-btn>
           <v-list-item class="w-100">
             <template v-slot:prepend>
               <font-awesome-icon class="mr-4 text-red" icon="fa-solid fa-heart"></font-awesome-icon>
             </template>
-
             <v-list-item-title class="poppins-light">{{ content.name }}</v-list-item-title>
             <v-list-item-subtitle class="poppins-light">{{
               content.relation
